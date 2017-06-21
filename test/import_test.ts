1e36c80706d28bfeb6f5dba3545a4bf632a2f04c
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-06-21 13:50:35 +0200

    adapted import tests

--- test/import_test.ts
+++ test/import_test.ts
@@ -6,11 +6,12 @@ describe('Merge imports with merge()', () => {
   let testResources = './test/resources/import/';
   let baseTestResources = testResources + 'base/';
   let patchTestResources = testResources + 'patch/';
+  let outputTestTempResources = testResources + 'output/';
   it('should accumulate imports from disjunct import sets (resources/import/{base|patch}/imports_1.ts)', () => {
     /**
      * fails if not all 4 import statements are present in the result
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_1.ts', patchTestResources + 'imports_1.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_1.ts', patchTestResources + 'imports_1.ts', outputTestTempResources + 'import_1_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length, "to contain all imports").to.equal(4);
     expect(result.map(value => value == "import { a } from 'b';").reduce((previous, current) => previous || current, false), "to contain the first import from base").to.equal(true);
     expect(result.map(value => value == "import { thit as that } from 'e';").reduce((previous, current) => previous || current, false), "to contain the second import from base").to.equal(true);
@@ -21,7 +22,7 @@ describe('Merge imports with merge()', () => {
     /**
      * fails if not all 4 import statements are present in the result
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_1.ts', patchTestResources + 'imports_1.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_1.ts', patchTestResources + 'imports_1.ts', outputTestTempResources + 'import_1_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length, "to contain all imports").to.equal(4);
     expect(result.map(value => value == "import { a } from 'b';").reduce((previous, current) => previous || current, false), "to contain the first import from base").to.equal(true);
     expect(result.map(value => value == "import { thit as that } from 'e';").reduce((previous, current) => previous || current, false), "to contain the second import from base").to.equal(true);
@@ -32,7 +33,7 @@ describe('Merge imports with merge()', () => {
     /**
      * fails if the result contains the same import twice (one time from base, one time from patch)
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_2.ts', patchTestResources + 'imports_2.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_2.ts', patchTestResources + 'imports_2.ts', outputTestTempResources + 'import_2_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length, "to contain unique imports").to.equal(3);
     expect(result.filter(value => value == "import { a } from 'b';").length).to.equal(1);
   });
@@ -40,7 +41,7 @@ describe('Merge imports with merge()', () => {
     /**
      * fails if the result contains the same import twice (one time from base, one time from patch)
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_2.ts', patchTestResources + 'imports_2.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_2.ts', patchTestResources + 'imports_2.ts', outputTestTempResources + 'import_2_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length, "to contain unique imports").to.equal(3);
     expect(result.filter(value => value == "import { a } from 'b';").length).to.equal(1);
   });
@@ -48,7 +49,7 @@ describe('Merge imports with merge()', () => {
     /**
      * fails if the import merge yields more than one import statement or doesn't contain the imported artefacts from base and patch
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_3.ts', patchTestResources + 'imports_3.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_3.ts', patchTestResources + 'imports_3.ts', outputTestTempResources + 'import_3_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length, "to contain only one import statement").to.equal(1);
     let importRegex = new RegExp(".*\{ [a,b], [a,b] \}.*");
     expect(importRegex.test(result[0].toString())).to.be.true;
@@ -57,7 +58,7 @@ describe('Merge imports with merge()', () => {
     /**
      * fails if the import merge yields more than one import statement or doesn't contain the imported artefacts from base and patch
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_3.ts', patchTestResources + 'imports_3.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_3.ts', patchTestResources + 'imports_3.ts', outputTestTempResources + 'import_3_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length, "to contain only one import statement").to.equal(1);
     let importRegex = new RegExp(".*\{ [a,b], [a,b] \}.*");
     expect(importRegex.test(result[0].toString()), "to contain both imported artifacts from the module").to.be.true;
@@ -66,28 +67,28 @@ describe('Merge imports with merge()', () => {
     /**
      * fails if the base import isn't present in the result.
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_4.ts', patchTestResources + 'imports_4.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_4.ts', patchTestResources + 'imports_4.ts', outputTestTempResources + 'import_4_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
   });
   it('should accumulate the imports from the base if the patch is empty and patchOverride is set. This is a borderline case (resources/import/{base|patch}/imports_4.ts)', () => {
     /**
      * fails if the base import isn't present in the result.
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_4.ts', patchTestResources + 'imports_4.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_4.ts', patchTestResources + 'imports_4.ts', outputTestTempResources + 'import_4_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
   });
   it('should accumulate the imports from the patch if the base is empty. This is a borderline case (resources/import/{base|patch}/imports_5.ts)', () => {
     /**
      * fails if the patch import isn't present in the result.
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_5.ts', patchTestResources + 'imports_5.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_5.ts', patchTestResources + 'imports_5.ts', outputTestTempResources + 'import_5_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
   });
   it('should accumulate the imports from the patch if the base is empty and patchOverride is set. This is a borderline case (resources/import/{base|patch}/imports_5.ts)', () => {
     /**
      * fails if the patch import isn't present in the result.
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_5.ts', patchTestResources + 'imports_5.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_5.ts', patchTestResources + 'imports_5.ts', outputTestTempResources + 'import_5_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
   });
 
@@ -97,7 +98,7 @@ describe('Merge imports with merge()', () => {
      * fails if the renaming of the patch is used
      * currently not supported
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_6.ts', patchTestResources + 'imports_6.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_6.ts', patchTestResources + 'imports_6.ts', outputTestTempResources + 'import_6_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
     let importRegex = new RegExp('.*\{ a as b \}.*');
     expect(importRegex.test(result[0].toString())).to.be.true;
@@ -107,7 +108,7 @@ describe('Merge imports with merge()', () => {
      * fails if the renaming of the base is used
      * currently not supported
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_6.ts', patchTestResources + 'imports_6.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_6.ts', patchTestResources + 'imports_6.ts', outputTestTempResources + 'import_6_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
     let importRegex = new RegExp('.*\{ a as d \}.*');
     expect(importRegex.test(result[0].toString())).to.be.true;
@@ -117,7 +118,7 @@ describe('Merge imports with merge()', () => {
      * fails if the import artifact of the patch is used
      * currently not supported
      */
-    const result : String[] = merge(false, baseTestResources + 'imports_7.ts', patchTestResources + 'imports_7.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(false, baseTestResources + 'imports_7.ts', patchTestResources + 'imports_7.ts', outputTestTempResources + 'import_7_output.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
     let importRegex = new RegExp('.*\{ a as b \}.*');
     expect(importRegex.test(result[0].toString())).to.be.true;
@@ -127,7 +128,7 @@ describe('Merge imports with merge()', () => {
      * fails if the import artifact of the base is used
      * currently not supported
      */
-    const result : String[] = merge(true, baseTestResources + 'imports_7.ts', patchTestResources + 'imports_7.ts').split('\n').filter(r => {return r.trim() != ""});
+    const result : String[] = merge(true, baseTestResources + 'imports_7.ts', patchTestResources + 'imports_7.ts', outputTestTempResources + 'import_7_output_override.ts', 'UTF-8').split('\n').filter(r => {return r.trim() != ""});
     expect(result.length).to.be.equal(1);
     let importRegex = new RegExp('.*\{ d as b \}.*');
     expect(importRegex.test(result[0].toString())).to.be.true;
