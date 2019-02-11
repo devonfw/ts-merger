8fa2aabb69b35c4d01d4c59c0a325b928d573b8b
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2019-02-11 12:23:22 +0100

    Disabling uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        filename: 'ts-merger.umd.js',
-        path: __dirname + '/dist',
-        libraryTarget: "umd",
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    node: {
-        fs: "empty",
-        child_process: 'empty'
-    }
+  entry: './src/index.ts',
+  output: {
+    filename: 'ts-merger.umd.js',
+    path: __dirname + '/dist',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  node: {
+    fs: 'empty',
+    child_process: 'empty',
+  },
 };
